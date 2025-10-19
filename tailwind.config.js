75512dd6a1c1a3286ce766ea797b3f4602b7e27a
Buttons Technology <buttonsafrotech@gmail.com>
2025-10-19 17:02:58 +0100

    #makeithappen

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        // Defines a custom utility class based on the 'Inter' font loaded in _app.js
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
