27f458dd85655a2150c674c709c0f05113bb8eae
Buttons Technology <buttonsafrotech@gmail.com>
2025-12-28 19:39:54 +0100

    addded an african banner

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,11 @@ import ProductDiscovery from './components/ProductDiscovery';
 export default function Home() {
   return (
     <div className="max-w-md mx-auto bg-slate-50 min-h-screen pb-20 font-sans">
-      <header className="p-6 bg-white border-b border-slate-100 flex justify-between items-center sticky top-0 z-50">
+      <header style={{backgroundImage: "url(/img/tribal.jpg)"}} className="p-6 bg-white border-b border-slate-100 flex justify-between items-center sticky top-0 z-50">
+      <div className='bg-white p-2 '>
+
         <h1 className="text-xl font-bold text-slate-800 tracking-tight">BUTTONS</h1>
+      </div>
         <div className="w-10 h-10 rounded-full bg-blue-100 flex items-center justify-center text-blue-600 font-bold">O</div>
       </header>
 
